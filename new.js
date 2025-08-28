b4338c01e30fa53dcf8b3f0d6a920528594dbff1
saksham_singh <sakshamsingh1307@gmail.com>
2025-08-28 15:13:16 +0530

    hie

---
+++ new.js
@@ -0,0 +1,9 @@
+function data(){
+    var a = document.getElementById("n1").value;
+    var b = document.getElementById("n2").value;
+    if(a==""||b=="" )
+    {
+        alert("Please fill the details");
+    }
+
+}
