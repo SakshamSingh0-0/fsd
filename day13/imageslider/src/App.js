4db23858acede00410a64dc8866f480b9cc24b1a
saksham_singh <sakshamsingh1307@gmail.com>
2025-11-03 15:33:17 +0530

    messagegdhf

---
+++ day13/imageslider/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Count from "./count"; // Count.jsx file ko import karo
+
+function App() {
+  return (
+    <div>
+      <Count /> {/* Count component ko yahan use karo */}
+    </div>
+  );
+}
+
+export default App;
